8dbd631a8fb03818beef03e0084ca65dad75e437
Garv <garvwashere@gmail.com>
2021-04-25 23:52:02 +0530

    apollo client setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import client from './apollo';
 import App from './App';
 import './index.css';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
