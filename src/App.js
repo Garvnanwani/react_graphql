8e80379a09b700d3422544672ce62d66e9713132
Garv <garvwashere@gmail.com>
2021-04-25 23:29:37 +0530

    basic router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      "Hello Appolo"
-    </div>
+    <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/:id" component={Detail} />
+    </Router>
   );
 }
 
