b7f839704f069a47e6b428f076f01bb9792eb52e
Garv <garvwashere@gmail.com>
2021-05-02 00:16:40 +0530

    added hosted backend

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost'
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000"
+    uri: "https://movie-api-graphql.vercel.app"
 })
 
 export default client
