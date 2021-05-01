5cd1143b1db8fe5aa60149a294d655c9c6a25231
Garv <garvwashere@gmail.com>
2021-05-02 01:13:18 +0530

    wrote first graphql query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,19 @@
-export default () => "Home"
+import { useQuery } from "@apollo/client"
+import { gql } from "apollo-boost"
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`
+
+export default () => {
+    const {loading, error, data} = useQuery(GET_MOVIES)
+    console.log(loading, error, data);
+    return (
+        <h1>Home</h1>
+    )
+}
